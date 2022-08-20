0d5f75bfb1a46e492b97e130104bd4fdd8abf95d
Kevan Williams <kevwilliams012@gmail.com>
2022-08-20 15:46:34 -0500

    spacemap fix

--- components/SpaceMap.js
+++ components/SpaceMap.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Space Map() {
+export default function SpaceMap() {
     return (
         <div id='spacemap' className='bg-pattern pt-5 pb-12 px-4 flex flex-col justify-between'>
             <h2 className='text-center text-6xl'>Space Map</h2>
-            
+
             <img src='./images/roadmap.png' />
 
 
