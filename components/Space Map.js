b86dca528a07d7d256473e68c39a3315683b1879
Kevan Williams <kevwilliams012@gmail.com>
2022-08-23 15:03:39 -0500

    change back js files

--- components/Space Map.js
+++ components/Space Map.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Space Map() {
+export default function SpaceMap() {
     return (
         <div id='spacemap' className='bg-pattern -mr-14 md:mr-0 pt-5 pb-12 px-4 flex flex-col justify-between'>
             <h2 className='text-center text-6xl'>Space Map</h2>
