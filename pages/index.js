87db2c484a15f1501ec92a6fc7ad0567c3e6aeb6
Kevan Williams <kevwilliams012@gmail.com>
2022-08-25 15:48:33 -0500

    add footer

--- pages/index.js
+++ pages/index.js
@@ -366,7 +366,7 @@ export default function Home() {
               <h3 className='text-center text-brightyellow font-semibold'>For every NFT minted, TLB creators will automatically burn 1,000,000 Shiba Inu tokens</h3>
 
               <button
-                className='connect titanium text-xs md:text-md mt-5 font-semibold uppercase font-base text-white px-1 md:px-3 py-2 border-2 border-teal rounded-md tracking-wide w-1/2 md:w-1/3 lg:w-1/4 hover:shadow-green-500/20'
+                className='connect titanium text-xs md:text-md mt-5 font-semibold uppercase font-base text-white px-2 md:px-3 py-2 border-2 border-teal rounded-md tracking-wide w-1/2 md:w-1/3 lg:w-1/4 hover:shadow-green-500/20'
               // onClick={mintPass}
 
               >
@@ -458,7 +458,7 @@ export default function Home() {
       {/*
         <img className="fixed bottom-0 left-0 w-1/5 z-20" src="/images/mainbull.png" />
         {/*<img className="fixed top-5 right-5 w-1/8 z-20" src="/images/spaceship.png" />*/}
-      {/*}     </section>
+      {/*     </section>
 
       <Benefits />
       <Backstory />
@@ -468,11 +468,12 @@ export default function Home() {
       <GiveBack />
 
       <Team />
+  */}
 
       <Footer />
-      
 
-      
+
+
 
 
 
