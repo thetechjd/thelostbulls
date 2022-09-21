c3b810b43166ab79a6d8d227dd391b6e8b367ee4
Kevan Williams <kevwilliams012@gmail.com>
2022-09-21 18:48:22 -0500

    presale button

--- pages/index.js
+++ pages/index.js
@@ -366,11 +366,11 @@ export default function Home() {
               <h3 className='text-center text-brightyellow font-semibold'>For every NFT minted, TLB creators will automatically burn 1,000,000 Shiba Inu tokens</h3>
 
               <button
-                className='connect titanium text-xs md:text-md mt-5 font-semibold uppercase font-base text-white px-2 md:px-3 py-2 border-2 border-teal rounded-md tracking-wide w-3/4 md:w-1/3 lg:w-1/4 hover:shadow-green-500/20'
+                className='connect titanium text-xs md:text-md mt-5 font-semibold uppercase font-base text-white px-3 md:px-4 py-2 border-2 border-teal rounded-md tracking-wide w-3/4 md:w-1/3 lg:w-1/4 hover:shadow-green-500/20'
               // onClick={mintPass}
 
               >
-                Join Pre-Sale Whitelist
+                Join Pre-Sale Whitelist 0.05 ETH
               </button>
             </div>
 
